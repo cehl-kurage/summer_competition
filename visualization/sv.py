a797d429934b7ceea972365e945143dfe0cf56b3
cehl_kurage <yusakuishihira@gmail.com>
2022-08-20 19:07:07 +0900

    パスを修正

--- visualization/sv.py
+++ visualization/sv.py
@@ -2,11 +2,9 @@ import pandas as pd
 import sweetviz as sv
 import pathlib
 
-data_directory = pathlib.Path("/home/yusaku/projects/summer_competition/data")
-train_path = data_directory.joinpath("train.csv")
-print(train_path, data_directory)
-train = pd.read_csv()
-test = pd.read_csv("../data/test.csv")
+data_dir = "/home/yusaku/projects/summer_competition/data/"
+train = pd.read_csv(data_dir + "train.csv")
+test = pd.read_csv(data_dir + "test.csv")
 
 report = sv.compare([train, "train"], [test, "test"], "failure")
 report.show_html("report.html")
