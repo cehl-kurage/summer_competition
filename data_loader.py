488b84f535669c66a6161f80709a08be90cfbad0
cehl_kurage <yusakuishihira@gmail.com>
2022-08-23 14:43:53 +0900

    optionを引数に含めた

--- data_loader.py
+++ data_loader.py
@@ -4,9 +4,17 @@ import pandas as pd
 data_dir = Path(__file__).resolve().parent.joinpath("data")
 
 
-def data_loader():
-    train_csv = data_dir.joinpath("train.csv")
-    test_csv = data_dir.joinpath("test.csv")
-    train = pd.read_csv(train_csv)
-    test = pd.read_csv(test_csv)
+def data_loader(option: str):
+    """
+    option: str data以下でさらにディレクトリを指定するとき
+
+    return: dict trainデータとtestデータの辞書
+    """
+    if option is None:
+        train = pd.read_csv(data_dir.joinpath("train.csv"))
+        test = pd.read_csv(data_dir.joinpath("test.csv"))
+    else:
+        train = pd.read_csv(data_dir.joinpath(f"{option}/train.csv"))
+        test = pd.read_csv(data_dir.joinpath(f"{option}/test.csv"))
+
     return {"train": train, "test": test}
