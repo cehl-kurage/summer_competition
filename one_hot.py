b5a79bc03c1421cf9088ede38d2ca9863463ada3
cehl_kurage <yusakuishihira@gmail.com>
2022-08-23 15:32:45 +0900

    train,testそれぞれに含まれない列があったので手動で追加した

--- one_hot.py
+++ one_hot.py
@@ -1,5 +1,7 @@
 from data_loader import data_loader
 import pandas as pd
+import numpy as np
+
 
 dataset = data_loader()
 train, test = dataset["train"], dataset["test"]
@@ -7,6 +9,12 @@ train, test = dataset["train"], dataset["test"]
 column_onehot = ["attribute_0", "attribute_1"]
 train_dummied = pd.get_dummies(train, columns=column_onehot)
 test_dummied = pd.get_dummies(test, columns=column_onehot)
+
+# attribute_1 == "material_7"の列がtrainにはないので、追加しておく
+train_dummied["attribute_1_material_7"] = 0
+
+# attribute_1 == "material_8"の列がtestにはないので、追加しておく
+test_dummied["attribute_1_material_8"] = 0
 
 # 頻繁に実行するファイルじゃないからパス書き込んじゃった
 train_dummied.to_csv("data/onehot/train.csv", index=False)
